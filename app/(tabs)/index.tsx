1678aeec4d624a2d0f74d93cffe2d4496765145f
Gektor <bheiko@salerow.software>
2024-05-17 17:13:07 +0300

    implemeent secondary slider, setup svg

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
 import MainBanner from "@/assets/images/mainBanner/main_banner.png";
 import SecondaryBanner from "@/assets/images/secondaryBanner/book_cover.png";
 
+import LockIcon from "@/assets/images/icons/lock.svg";
 import { DEFAULT_COLORS } from "@/constants/Colors";
 import { FONTS } from "@/constants/fonts";
+import { Image } from "expo-image";
 import {
-  Image,
   ImageBackground,
   ScrollView,
   StyleSheet,
@@ -12,6 +13,31 @@ import {
   View,
   useWindowDimensions,
 } from "react-native";
+
+const DECONDARy_SLIDER: Slider[] = [
+  {
+    id: 1,
+    img: SecondaryBanner,
+    title: "Wolfstate chronicles: Alaska, Texas",
+    isCommingSoon: false,
+  },
+  {
+    id: 2,
+    img: SecondaryBanner,
+    title: "Wolfstate chronicles: Alaska, Texas",
+    isCommingSoon: true,
+    isCommingTitile: "Beautiful Revenge",
+    isCommingTitileDate: "Coming July 2",
+  },
+  {
+    id: 3,
+    img: SecondaryBanner,
+    title: "Wolfstate chronicles: Alaska, Texas",
+    isCommingSoon: true,
+    isCommingTitile: "Sin De Rella",
+    isCommingTitileDate: "Coming July 2",
+  },
+];
 
 export default function HomeScreen() {
   return (
@@ -23,10 +49,20 @@ export default function HomeScreen() {
       <View style={{ gap: 44 }}>
         <MainSlider />
         <SecondarySlider />
+        <SecondarySlider />
       </View>
     </ScrollView>
   );
 }
+
+type Slider = {
+  id: number;
+  img: typeof SecondaryBanner;
+  title: string;
+  isCommingSoon: boolean;
+  isCommingTitileDate?: string;
+  isCommingTitile?: string;
+};
 
 const SecondarySlider = () => {
   const { width } = useWindowDimensions();
@@ -50,53 +86,74 @@ const SecondarySlider = () => {
         style={{ gap: 12 }}
       >
         <View style={{ flexDirection: "row", gap: 12 }}>
-          <View
-            style={{
-              width: width / 3,
-              gap: 8,
-            }}
-          >
+          {DECONDARy_SLIDER.map((slide) => (
             <View
+              key={slide.id}
               style={{
-                borderRadius: 8,
-                overflow: "hidden",
+                width: width / 3,
+                gap: 8,
               }}
             >
-              <Image
-                source={SecondaryBanner}
+              <View
                 style={{
-                  resizeMode: "stretch",
-                  aspectRatio: 120 / 150,
-                  width: "100%",
-                  height: undefined,
+                  borderRadius: 8,
+                  overflow: "hidden",
+                  position: "relative",
                 }}
-              />
-            </View>
+              >
+                <Image
+                  source={slide.img}
+                  blurRadius={slide.isCommingSoon ? 10 : 0}
+                  style={{
+                    aspectRatio: 120 / 150,
+                    width: "100%",
+                    height: undefined,
+                  }}
+                />
+                {slide.isCommingSoon && (
+                  <View
+                    style={{
+                      position: "absolute",
+                      top: 60,
+                      left: 45,
+                      borderRadius: 100,
+                      backgroundColor: "#FFFFFF52",
+                      // opacity: 0.3,
+                      padding: 10,
+                    }}
+                  >
+                    <LockIcon />
+                  </View>
+                )}
+              </View>
 
-            <View style={{ paddingRight: "20%" }}>
-              <Text
-                style={{
-                  color: DEFAULT_COLORS.electricBlue,
-                  fontFamily: FONTS.NunitoExtraBold800,
-                  textTransform: "uppercase",
-                  fontSize: 11,
-                  lineHeight: 14,
-                }}
-              >
-                Coming July 2
-              </Text>
-              <Text
-                style={{
-                  fontFamily: FONTS.NunitoSemiBold600,
-                  fontSize: 14,
-                  lineHeight: 17,
-                  color: DEFAULT_COLORS.white,
-                }}
-              >
-                Wolfstate chronicles: Alaska, Texas
-              </Text>
+              <View style={{ paddingRight: "20%" }}>
+                {slide.isCommingSoon && (
+                  <Text
+                    style={{
+                      color: DEFAULT_COLORS.electricBlue,
+                      fontFamily: FONTS.NunitoExtraBold800,
+                      textTransform: "uppercase",
+                      fontSize: 11,
+                      lineHeight: 14,
+                    }}
+                  >
+                    {slide.isCommingTitileDate}
+                  </Text>
+                )}
+                <Text
+                  style={{
+                    fontFamily: FONTS.NunitoSemiBold600,
+                    fontSize: 14,
+                    lineHeight: 17,
+                    color: DEFAULT_COLORS.white,
+                  }}
+                >
+                  {slide.isCommingSoon ? slide.isCommingTitile : slide.title}
+                </Text>
+              </View>
             </View>
-          </View>
+          ))}
         </View>
       </ScrollView>
     </View>
