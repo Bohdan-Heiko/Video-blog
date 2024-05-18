fb2caf01fe3f7f84f8263071b56745dc76403bfc
Gektor <bheiko@salerow.software>
2024-05-18 04:46:35 +0300

    refactoring secondary slider

---
+++ components/seconddary-slider/style/style.ts
@@ -0,0 +1,64 @@
+import { DEFAULT_COLORS } from "@/constants/Colors";
+import { FONTS } from "@/constants/fonts";
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+  mainContainer: {
+    gap: 16,
+  },
+
+  mainTitle: {
+    fontFamily: FONTS.NunitoBold700,
+    color: DEFAULT_COLORS.white,
+    fontSize: 20,
+    lineHeight: 24,
+  },
+
+  slider: {
+    gap: 12,
+  },
+
+  cardContainer: {
+    flexDirection: "row",
+    gap: 12,
+  },
+  card: {
+    gap: 8,
+  },
+  imgContainer: {
+    borderRadius: 8,
+    overflow: "hidden",
+    position: "relative",
+  },
+  img: {
+    aspectRatio: 120 / 150,
+    width: "100%",
+    height: undefined,
+  },
+  lockContainer: {
+    position: "absolute",
+    top: 60,
+    left: 45,
+    borderRadius: 100,
+    backgroundColor: "#FFFFFF52",
+    padding: 10,
+  },
+
+  textContainer: {
+    paddingRight: "20%",
+  },
+
+  comminSoongTitle: {
+    color: DEFAULT_COLORS.electricBlue,
+    fontFamily: FONTS.NunitoExtraBold800,
+    textTransform: "uppercase",
+    fontSize: 11,
+    lineHeight: 14,
+  },
+  title: {
+    fontFamily: FONTS.NunitoSemiBold600,
+    fontSize: 14,
+    lineHeight: 17,
+    color: DEFAULT_COLORS.white,
+  },
+});
--- screens/main/index.tsx
+++ screens/main/index.tsx
@@ -1,4 +1,5 @@
 import { MainSlider } from "@/components/main-slider";
+import { SecondarySlider } from "@/components/seconddary-slider";
 import { ScrollView } from "react-native";
 import { style } from "./style";
 
@@ -6,6 +7,7 @@ export const Home = () => {
   return (
     <ScrollView overScrollMode="never" showsVerticalScrollIndicator={false} style={style.main}>
       <MainSlider />
+      <SecondarySlider />
     </ScrollView>
   );
 };
