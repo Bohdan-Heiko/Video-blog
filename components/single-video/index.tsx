e6c520759c7cb84b09c07b2eea0b05631da6a263
Gektor <bheiko@salerow.software>
2024-05-18 08:39:52 +0300

    setup times slider, fix styles

--- components/single-video/index.tsx
+++ components/single-video/index.tsx
@@ -1,11 +1,15 @@
-import { AVPlaybackStatus, ResizeMode, Video } from "expo-av";
+import { AVPlaybackStatusSuccess, ResizeMode, Video } from "expo-av";
 import { useEffect, useRef, useState } from "react";
 import { Pressable, StyleSheet, Text, View, useWindowDimensions } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-// import { style } from "./style/style";
+
 import CrossIcon from "@/assets/images/icons/cross.svg";
+import { DEFAULT_COLORS } from "@/constants/Colors";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import { Slider } from "@miblanchard/react-native-slider";
 import { LinearGradient } from "expo-linear-gradient";
+import { style } from "./style/style";
+
 type VideoType = {
   videoData: {
     id: number;
@@ -31,7 +35,7 @@ export const SingleVideo = ({ videoData, activeVideoId }: VideoType) => {
   const { height } = useWindowDimensions();
 
   const [isSeeking, setIsSeeking] = useState(false);
-  const [status, setStatus] = useState<AVPlaybackStatus>();
+  const [status, setStatus] = useState<AVPlaybackStatusSuccess>();
   const [duration, setDuration] = useState<number>(0);
   const [sliderValue, setSliderValue] = useState(0);
 
@@ -48,14 +52,24 @@ export const SingleVideo = ({ videoData, activeVideoId }: VideoType) => {
     }
   };
 
-  const handleValueChange = (value) => {
-    console.log(value);
+  const handleSlidingStart = () => {
+    setIsSeeking(true);
+  };
+
+  const handleValueChange = (value: Array<number>) => {
     setSliderValue(value[0]);
   };
 
-  const handleSlidingComplete = async (value) => {
+  const setVideoPosition = async (position: number) => {
     if (videoRef.current) {
-      await videoRef.current.setPositionAsync(value[0] * status.durationMillis);
+      await videoRef.current.setPositionAsync(position);
+    }
+  };
+
+  const handleSlidingComplete = async (value: Array<number>) => {
+    if (videoRef.current && status?.durationMillis) {
+      setVideoPosition(value[0] * status.durationMillis);
+      // await videoRef.current.setPositionAsync(value[0] * status.durationMillis);
       setIsSeeking(false);
     }
   };
@@ -70,28 +84,32 @@ export const SingleVideo = ({ videoData, activeVideoId }: VideoType) => {
     }
 
     if (activeVideoId === videoData.id) {
+      setSliderValue(0);
+      setVideoPosition(0);
+
       videoRef.current?.playAsync();
     }
   }, [activeVideoId]);
+  // console.log(Dimensions.get("window").width - 16 - 32);
+
   return (
-    <View style={{ position: "relative" }}>
+    <View style={style.mainContainer}>
       <View style={{ height }}>
         <Video
           ref={videoRef}
           style={[StyleSheet.absoluteFill]}
           source={{ uri: videoData.url }}
           resizeMode={ResizeMode.COVER}
-          // onPlaybackStatusUpdate={setStatus}
           onPlaybackStatusUpdate={(status) => {
-            setStatus(() => status);
-            if (!isSeeking && status.isLoaded) {
-              setSliderValue(status.positionMillis / status.durationMillis);
+            setStatus(status as AVPlaybackStatusSuccess);
+            if (!isSeeking && status.isLoaded && status.durationMillis) {
+              setSliderValue(status.positionMillis / status?.durationMillis);
             }
           }}
         />
-        <Pressable onPress={onPlay} style={{ flex: 1 }}>
-          <View style={{ flex: 1 }}>
-            <SafeAreaView style={{ flex: 1 }}>
+        <Pressable onPress={onPlay} style={style.flex1}>
+          <View style={style.flex1}>
+            <SafeAreaView style={style.flex1}>
               <LinearGradient
                 colors={[
                   "rgba(0, 0, 0, 0.9)",
@@ -100,32 +118,14 @@ export const SingleVideo = ({ videoData, activeVideoId }: VideoType) => {
                   "rgba(0, 0, 0, 0)",
                 ]}
                 locations={[0.4, 0.6, 0.8, 1]}
-                style={{
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                  right: 0,
-                  height: 100,
-                }}
+                style={style.topGradient}
               />
-              <View style={{ justifyContent: "space-between", flex: 1 }}>
-                <View
-                  style={{
-                    paddingHorizontal: 20,
-                    // gap: width / 3 - 10,
-                    flexDirection: "row",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    position: "relative",
-                  }}
-                >
-                  <CrossIcon style={{ position: "absolute", left: 20 }} />
-                  <Text style={{ fontSize: 30, color: "white" }}>{videoData.id}</Text>
+              <View style={style.labelVideoContainer}>
+                <View style={style.label}>
+                  <CrossIcon style={style.icon} />
+                  <Text style={style.title}>{videoData.id}</Text>
                 </View>
-
-                {/* Playback slider */}
               </View>
-              {/* <Text style={{ fontSize: 30, color: "white" }}>{isPlayng ? "Play" : "Pause"}</Text> */}
 
               <LinearGradient
                 colors={[
@@ -135,75 +135,43 @@ export const SingleVideo = ({ videoData, activeVideoId }: VideoType) => {
                   "rgba(0, 0, 0, 0.9)",
                 ]}
                 locations={[0.4, 0.6, 0.8, 1]}
-                style={{
-                  position: "absolute",
-                  bottom: 0,
-                  left: 0,
-                  right: 0,
-                  height: 100,
-                }}
+                style={style.bottomGradient}
               />
             </SafeAreaView>
           </View>
         </Pressable>
       </View>
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "center",
-          width: "100%",
-          // alignSelf: "center",
-          // backgroundColor: "black",
-          padding: 10,
-          position: "absolute",
-          bottom: 10,
-          gap: 15,
-        }}
-      >
-        <Text style={{ color: "white", fontSize: 12 }}>{formatTime(status?.positionMillis)}</Text>
 
-        <Slider
-          value={sliderValue}
-          onValueChange={handleValueChange}
-          // onSlidingStart={handleSlidingStart}
-          onSlidingComplete={handleSlidingComplete}
-          minimumTrackTintColor="#FFFFFF"
-          maximumTrackTintColor="#000000"
-          thumbTintColor="#FFFFFF"
-          containerStyle={styles.slider}
-        />
-
-        <Text style={{ color: "white", fontSize: 12 }}>{formatTime(status?.durationMillis)}</Text>
+      <View style={style.timeContainer}>
+        <View style={style.sliderMainContainer}>
+          <Pressable onPress={onPlay}>
+            {isPlayng ? (
+              <FontAwesome6 name="pause" size={24} color="white" />
+            ) : (
+              <FontAwesome6 name="play" size={24} color="white" />
+            )}
+          </Pressable>
+          <View style={style.sliderContainer}>
+            <Text style={[style.sliderTime, { position: "absolute", bottom: -4 }]}>
+              {formatTime(status?.positionMillis as number)}
+            </Text>
+            <Slider
+              value={sliderValue}
+              onValueChange={handleValueChange}
+              onSlidingStart={handleSlidingStart}
+              onSlidingComplete={handleSlidingComplete}
+              minimumTrackTintColor="#FFFFFF"
+              maximumTrackTintColor={"rgba(255, 255, 255, 0.3)"}
+              thumbTintColor={DEFAULT_COLORS.white}
+              containerStyle={style.slider}
+              thumbStyle={{ height: 9, width: 9 }}
+            />
+            <Text style={[style.sliderTime, { position: "absolute", bottom: -4, right: 0 }]}>
+              {formatTime(status?.durationMillis as number)}
+            </Text>
+          </View>
+        </View>
       </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#000",
-  },
-  video: {
-    width: "100%",
-    height: 300,
-  },
-  slider: {
-    width: "70%",
-    // marginTop: 10,
-    // position: "absolute",
-    // bottom: 20,
-  },
-  timeContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    width: "90%",
-    marginTop: 5,
-  },
-  timeText: {
-    color: "#FFFFFF",
-  },
-});
--- components/single-video/style/style.ts
+++ components/single-video/style/style.ts
@@ -1,3 +1,96 @@
 import { StyleSheet } from "react-native";
 
-export const style = StyleSheet.create({});
+export const style = StyleSheet.create({
+  mainContainer: {
+    position: "relative",
+  },
+
+  flex1: {
+    flex: 1,
+  },
+
+  topGradient: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 100,
+  },
+
+  bottomGradient: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 100,
+  },
+
+  labelVideoContainer: {
+    justifyContent: "space-between",
+    flex: 1,
+  },
+
+  label: {
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "relative",
+  },
+
+  icon: {
+    position: "absolute",
+    left: 20,
+  },
+
+  title: {
+    fontSize: 30,
+    color: "white",
+  },
+
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000",
+  },
+  video: {
+    width: "100%",
+    height: 300,
+  },
+
+  timeContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    paddingHorizontal: 20,
+    paddingBottom: 15,
+    position: "absolute",
+    bottom: 15,
+    gap: 15,
+  },
+
+  sliderMainContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 20,
+  },
+
+  sliderContainer: {
+    flex: 1,
+    position: "relative",
+    gap: 4,
+  },
+
+  sliderTime: {
+    color: "white",
+    fontSize: 12,
+  },
+
+  slider: {
+    width: "100%",
+  },
+  timeText: {
+    color: "#FFFFFF",
+  },
+});
